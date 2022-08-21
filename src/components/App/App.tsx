e70402703e7c3b430e6611998728913607192b34
maaqoul <hicham.maaqoul@gmail.com>
2022-08-21 04:25:43 +0100

    add chakra ui and responsivetable component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,15 @@
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+
+import theme from "../../customTheme";
+import ResponsiveTable from "../ResponsiveTable";
 
 function App() {
   return (
-    <div >
-        Hello 
-    </div>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+      <div>List of Keywords</div>
+      <ResponsiveTable />
+    </ChakraProvider>
   );
 }
 
